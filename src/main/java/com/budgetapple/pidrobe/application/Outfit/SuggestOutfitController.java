bfe1f118a35891b8c75725996b897d0a83f2f7e1
Miguel Cardoso [1151418] <1151418@isep.ipp.pt>
2017-09-09 15:35:08 +0100

    Created base controller to generate an outfit

---
+++ src/main/java/com/budgetapple/pidrobe/application/Outfit/SuggestOutfitController.java
@@ -0,0 +1,20 @@
+package com.budgetapple.pidrobe.application.Outfit;
+
+import com.budgetapple.pidrobe.core.clothes.Outfit;
+
+/**
+ * Created by Miguel Cardoso on 09/09/2017.
+ */
+public class SuggestOutfitController {
+
+    private Outfit outfit;
+
+    public SuggestOutfitController() {
+
+    }
+
+    public Outfit generateSuggestedOutfit() {
+        //TODO Actually generate outfit
+        return outfit;
+    }
+}
