3ce32548d8756fc9f8bfbd5c19384e8b17f9891f
Luis Goncalves <luis.gnlvs@gmail.com>
2017-09-10 13:51:26 +0100

    Fix bootstrap

--- src/main/java/com/budgetapple/pidrobe/bootstrap/core/ItemBootstrap.java
+++ src/main/java/com/budgetapple/pidrobe/bootstrap/core/ItemBootstrap.java
@@ -23,23 +23,23 @@ public class ItemBootstrap {
     }
 
     private List<Item> generateItems() {
-        Item tshirt1 = new Item(10, imageBase64("green_shirt"), "#00ff00", Item.MAX_TEMP_INDEX, true);
-        Item tshirt2 = new Item(10, imageBase64("red_shirt"), "#ff0000", Item.MAX_TEMP_INDEX, true);
-        Item tshirt3 = new Item(10, imageBase64("blue_shirt"), "#0000ff", Item.MAX_TEMP_INDEX, true);
-        Item tshirt4 = new Item(10, imageBase64("white_shirt"), "#ffffff", Item.MAX_TEMP_INDEX, true);
-        Item tshirt5 = new Item(10, imageBase64("black_shirt"), "#000000", Item.MAX_TEMP_INDEX, true);
+        Item tshirt1 = new Item(-1,10, imageBase64("green_shirt"), "#00ff00", Item.MAX_TEMP_INDEX, true);
+        Item tshirt2 = new Item(-1, 10, imageBase64("red_shirt"), "#ff0000", Item.MAX_TEMP_INDEX, true);
+        Item tshirt3 = new Item(-1, 10, imageBase64("blue_shirt"), "#0000ff", Item.MAX_TEMP_INDEX, true);
+        Item tshirt4 = new Item(-1, 10, imageBase64("white_shirt"), "#ffffff", Item.MAX_TEMP_INDEX, true);
+        Item tshirt5 = new Item(-1, 10, imageBase64("black_shirt"), "#000000", Item.MAX_TEMP_INDEX, true);
 
-        Item jeans1 = new Item(9, imageBase64("jeans1"), "#1560BD", 3, true);
-        Item jeans2 = new Item(9, imageBase64("jeans2"), "#696969", 3, true);
-        Item jeans3 = new Item(9, imageBase64("jeans3"), "#000000", 3, true);
-        Item shorts1 = new Item(5, imageBase64("shorts1"), "#1560BD", 3, true);
-        Item shorts2 = new Item(5, imageBase64("shorts2"), "#000000", 3, true);
+        Item jeans1 = new Item(-1, 9, imageBase64("jeans1"), "#1560BD", 3, true);
+        Item jeans2 = new Item(-1, 9, imageBase64("jeans2"), "#696969", 3, true);
+        Item jeans3 = new Item(-1, 9, imageBase64("jeans3"), "#000000", 3, true);
+        Item shorts1 = new Item(-1, 5, imageBase64("shorts1"), "#1560BD", 3, true);
+        Item shorts2 = new Item(-1, 5, imageBase64("shorts2"), "#000000", 3, true);
 
-        Item shoes1 = new Item(4, imageBase64("shoes1"), "#000000", 3, true);
-        Item shoes2 = new Item(4, imageBase64("shoes2"), "#000000", 3, true);
-        Item shoes3 = new Item(4, imageBase64("shoes3"), "#111111", 3, true);
-        Item flipflop1 = new Item(2, imageBase64("flipflop1"), "#000000", 3, true);
-        Item flipflop2 = new Item(2, imageBase64("flipflop2"), "#00ff00", 3, true);
+        Item shoes1 = new Item(-1, 4, imageBase64("shoes1"), "#000000", 3, true);
+        Item shoes2 = new Item(-1, 4, imageBase64("shoes2"), "#000000", 3, true);
+        Item shoes3 = new Item(-1, 4, imageBase64("shoes3"), "#111111", 3, true);
+        Item flipflop1 = new Item(-1, 2, imageBase64("flipflop1"), "#000000", 3, true);
+        Item flipflop2 = new Item(-1, 2, imageBase64("flipflop2"), "#00ff00", 3, true);
 
         List<Item> itemList = new LinkedList<>();
         itemList.add(tshirt1);
